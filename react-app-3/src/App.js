2e71c8b227d19f2bee9c4ebf73588e2d75907f8a
Washington Soares <wsoaresfilho@hotmail.com.br>
2020-02-21 17:39:38 -0300

    Adding the react-app-3 frontend code

---
+++ react-app-3/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="App">
+      <div className="App-header">
+        { props.name && <h2>{`App: ${props.name}`}</h2> }
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          A third React application?! WOW!!!
+        </p>
+        <a
+          className="App-link"
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go back to homepage
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
